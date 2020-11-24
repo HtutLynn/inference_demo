d3d8d78fc4039338bf62cdcfca39aa4c32d2b675
Htut Lynn Aung <htutlynnaung.cs@gmail.com>
2020-11-24 18:36:52 +0630

    first commit.

---
+++ checkpoints/fetch_onnx_models.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+echo "Downloading onnx model file for custom YOLOv5 head models..."
+wget https://altotechpublic.s3-ap-southeast-1.amazonaws.com/naplabchula/models/custom_head_YOLOv5_1.onnx -q --show-progress --no-clobber
+wget https://altotechpublic.s3-ap-southeast-1.amazonaws.com/naplabchula/models/custom_head_YOLOv5_4.onnx -q --show-progress --no-clobber
+wget https://altotechpublic.s3-ap-southeast-1.amazonaws.com/naplabchula/models/custom_head_YOLOv5_6.onnx -q --show-progress --no-clobber
+
+
+echo "Done!"
